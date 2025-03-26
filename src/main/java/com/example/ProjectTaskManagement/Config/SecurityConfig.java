f26f3cd466af4daaf322edb01405ba404c34878f
Shrasti0207 <shrasti.gupta@nashtechglobal.com>
2025-03-26 13:07:26 +0530

    spring security

---
+++ src/main/java/com/example/ProjectTaskManagement/Config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.example.ProjectTaskManagement.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests)-> requests.requestMatchers(
+                "/users").authenticated()
+                .requestMatchers("/projects", "/tasks").permitAll());
+//        http.formLogin(formLogin -> formLogin.disable());
+        http.formLogin(withDefaults());
+        // sent the credentials in the header
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails userDetails = User.withUsername("gauri").password(passwordEncoder().encode("12345")).authorities("USER").build();
+        UserDetails admin = User.withUsername("admin").password("123456").authorities("ADMIN").build();
+        return new InMemoryUserDetailsManager(userDetails, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
